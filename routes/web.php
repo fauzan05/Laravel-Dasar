0003a946fd97281314734940828eb41828d7c807
Fauzan Nurhidayat <fauzannurhidayat@Fauzans-MacBook-Pro.local>
2023-10-10 14:17:32 +0700

    add : menambahkan beberapa file yang tertinggal karena terhapus

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,67 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pzn', function (){
+    return "Hello Programmer Zaman Now";
+});
+
+Route::redirect('/youtube', '/pzn');
+
+Route::fallback(function (){
+    return "404 by Programmer Zaman Now";
+});
+
+Route::view('/hello', 'hello', ['name' => 'Eko']);
+
+Route::get('/hello-again', function (){
+    return view('hello', ['name' => 'Eko']);
+});
+
+Route::get('/hello-world', function (){
+    return view('hello.world', ['name' => 'Eko']);
+});
+
+Route::get('/products/{id}', function ($productId){
+    return "Product $productId";
+})->name('product.detail');
+
+Route::get('/products/{product}/items/{item}', function ($productId, $itemId){
+    return "Product $productId, Item $itemId";
+})->name('product.item.detail');
+
+Route::get('/categories/{id}', function ($categoryId){
+    return "Category $categoryId";
+})->where('id', '[0-9]+')->name('category.detail');
+
+Route::get('/users/{id?}', function ($userId = '404'){
+    return "User $userId";
+})->name('user.detail');
+
+Route::get('/conflict/eko', function (){
+    return "Conflict Eko Kurniawan Khannedy";
+});
+
+Route::get('/conflict/{name}', function ($name){
+    return "Conflict $name";
+});
+
+Route::get('/produk/{id}', function ($id){
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/produk-redirect/{id}', function ($id){
+    return redirect()->route('product.detail', ['id' => $id]);
+});
+
+Route::get('/controller/hello/request', [\App\Http\Controllers\HelloController::class, 'request']);
+Route::get('/controller/hello/{name}', [\App\Http\Controllers\HelloController::class, 'hello']);
